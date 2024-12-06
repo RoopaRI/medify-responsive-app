1f6d18c797f4d69c022b439346711b08b8d99c3e
Roopa S <roopa31113@gmail.com>
2024-12-06 09:02:52 +0530

    Added footer and DownloadApp component. These 2 components are common for HomePage and Booking  Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {CssBaseline} from '@mui/material';
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
 function App() {
   return (
     <div>
       <CssBaseline />
       <Outlet />
+      <DownloadApp />
+      <Footer />
     </div>
   );
 }
